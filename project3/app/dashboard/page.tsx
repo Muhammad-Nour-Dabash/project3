9f05452e98f97bfd67b93e718caccf605659038c
Muhammad Nour Dabash <muhammad.nour@rightcode.io>
2025-05-30 10:39:14 +0300

    Done ^_^

--- project3/app/dashboard/page.tsx
+++ project3/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default async function DashboardPage() {
     <div className="bg-white shadow-lg rounded-xl p-6 space-y-4">
       <h1 className="text-2xl font-bold text-blue-600">📊 Dashboard</h1>
       <div className="space-y-2 border border-gray-200 rounded-lg p-4 bg-gray-50">
-        <h1 className="text-lg font-semibold">Public API example (SRR)</h1>
+        <h1 className="text-lg font-semibold">Public API example (SSR)</h1>
         <p className="text-gray-800">
           Message: <strong>{message}</strong>
         </p>
